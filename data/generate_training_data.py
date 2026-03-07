954b358758adfb75b21b12d3102fe255a0067537
Mateo MN <mateo.martin.nieto@gmail.com>
2026-03-07 15:33:12 +0000

    Clean Up training notebook

--- data/generate_training_data.py
+++ data/generate_training_data.py
@@ -1,9 +1,11 @@
 import random
+import os, json
+import requests
 import pandas as pd
-
-random.seed(42)
-import pandas as pd
-import random
+from Bio.PDB import PDBParser
+from io import StringIO
+from SafeFold_architecture.DPFunc_fork.DPFunctional import get_GO_terms
+from tqdm import tqdm
 
 def read_fasta(filepath):
     records = []
@@ -35,11 +37,11 @@ def read_fasta(filepath):
     return parsed
 
 # Load toxins
-toxins = read_fasta("./Toxins.fasta")
+toxins = read_fasta("./data/Toxins.fasta")
 n_toxins = len(toxins)
 
 # Load non-toxins
-non_toxins = read_fasta("./Non-toxins.fasta")
+non_toxins = read_fasta("./data/Non-toxins.fasta")
 
 if len(non_toxins) < n_toxins:
     raise ValueError("Not enough non-toxin sequences to match toxin count.")
@@ -55,9 +57,54 @@ data = pd.DataFrame({
     "sequence": [x[1] for x in all_data],
     "toxicity": [1]*n_toxins + [0]*n_toxins
 })
+data.head()
 
-# Shuffle dataset
-data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+parser = PDBParser(QUIET=True)
 
-print(data.head())
-print(f"\nTotal samples: {len(data)}")
+def generate_json(df, output_file):
+    done = set()
+
+    # load already-processed IDs from existing JSONL (skip malformed lines)
+    if os.path.exists(output_file):
+        with open(output_file, "r") as f:
+            for line in f:
+                line = line.strip()
+                if not line:
+                    continue
+                try:
+                    obj = json.loads(line)
+                except json.JSONDecodeError:
+                    continue
+
+                uid = obj.get("uniprot_id")
+                if uid:
+                    done.add(uid)
+                    
+    print(done)
+
+    with open(output_file, "a") as f:
+        for uniprot_id in tqdm(df["uniprot_id"].dropna().astype(str).unique()):
+            try:
+                print(uniprot_id)
+                # if the ID (UI/uid/uniprot_id) is already in the JSONL, skip
+                if uniprot_id in done:
+                    continue
+
+                af = requests.get(f"https://alphafold.ebi.ac.uk/api/prediction/{uniprot_id}").json()
+                pdb_string = requests.get(af[0]["pdbUrl"]).text
+
+                structure = parser.get_structure("protein", StringIO(pdb_string))
+                model = structure[0]
+
+                GO_terms = get_GO_terms(model, uniprot_id)
+
+                f.write(json.dumps({"uniprot_id": uniprot_id, "GO_terms": GO_terms.to_dict()}) + "\n")
+                f.flush()
+
+                done.add(uniprot_id)
+
+            except Exception:
+                continue
+            
+generate_json(data[data["toxicity"] == 1], "go_terms_toxic.json")
+generate_json(data[data["toxicity"] == 0], "go_terms_not_toxic.json")
